908ffd8c15d2c88968052d9c294b1dfa3b1f7485
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-04 10:09:07 -0500

    Added reset functionality, as well as a scoring system to the backend, integrated it to the dash app.

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from dash import dash_core_components as dcc
 from dash import dash_html_components as html
 from dash import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
+from game_logic import game
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
     dbc.Row([
@@ -10,7 +13,8 @@ app.layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col(dcc.Input(id="word-input", type="text", placeholder="Enter your guess"), width=8),
-        dbc.Col(dbc.Button("Submit", id="submit-button", color="primary"), width=4)
+        dbc.Col(dbc.Button("Submit", id="submit-button", color="primary"), width=2),
+        dbc.Col(dbc.Button("Reset", id="reset-button", color="secondary"), width=2)
     ]),
     dbc.Row([
         dbc.Col(html.Div(id="output-div"), width=12)
@@ -18,18 +22,33 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H3("Guess History"), className="text-center"),
         dbc.Col(html.Div(id="guess-history"), width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(html.Div(id="score-div"), width=12)
     ])
 ], fluid=True)
 
 @app.callback(
-    Output('output-div', 'children'),
-    [Input('submit-button', 'n_clicks')],
+    [Output('output-div', 'children'),
+     Output('guess-history', 'children'),
+     Output('score-div', 'children')],
+    [Input('submit-button', 'n_clicks'),
+     Input('reset-button', 'n_clicks')],
     [dash.dependencies.State('word-input', 'value')]
 )
-def update_output(n_clicks, value):
-    if n_clicks is None:
-        return ""
-    return f"You guessed: {value}"
+def update_output(submit_clicks, reset_clicks, value):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return "", "", ""
+    if ctx.triggered[0]['prop_id'] == 'reset-button.n_clicks':
+        game.reset()
+        return "", "", ""
+    if not value:
+        return "", "", ""
+    result = game.check_guess(value)
+    history = game.get_guesses()
+    score = f"Score: {game.score}"
+    return result, html.Ul([html.Li(guess) for guess in history]), score
 
 @app.callback(
     [Output('output-div', 'children'),
--- game_logic.py
+++ game_logic.py
@@ -4,14 +4,18 @@ class ContextoGame:
     def __init__(self):
         self.target_word = "example"
         self.guesses = []
+        self.score = 0
 
     def check_guess(self, guess):
         self.guesses.append(guess)
         if guess == self.target_word:
-            return f"{guess} is Correct!"
+            self.score += 1
+            return f"{guess} is Correct! Your score is {self.score}"
         else:
-            # Example feedback logic
             common_letters = set(guess) & set(self.target_word)
             return f"{guess} is not the word. Common letters: {', '.join(common_letters)}"
-
+        
+    def reset(self):
+        self.__init__()
+        
 game = ContextoGame()
