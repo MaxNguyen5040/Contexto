87dfb807b93f9f224ebb8b737a59ef77f3b79812
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-04 19:20:00 -0500

    Added guess analysis, as well as updating code.

--- app.py
+++ app.py
@@ -88,6 +88,55 @@ def update_similarity_output(selected_word):
         ])
     return html.Div("")
 
+@app.callback(
+    [dash.dependencies.Output('game-output', 'children'),
+     dash.dependencies.Output('score-output', 'children')],
+    [dash.dependencies.Input('btn-submit', 'n_clicks')],
+    [dash.dependencies.State('input-word', 'value')]
+)
+def update_game(n_clicks, input_word):
+    global user_score
+    if n_clicks and input_word:
+        similarity_scores = model.calculate_word_similarity([input_word] + words)
+        user_score += 10  # Example scoring logic
+        return html.Div(f"Similarity scores: {similarity_scores}"), html.Div(f"Score: {user_score}")
+    return html.Div(), html.Div()
+
+def analyze_guesses_and_update_state(guesses, target_word, similarity_threshold=0.5):
+    from difflib import SequenceMatcher
+    import random
+    
+    updated_guesses = []
+    game_over = False
+    message = ""
+    
+    def calculate_similarity(word1, word2):
+        return SequenceMatcher(None, word1, word2).ratio()
+    
+    for guess in guesses:
+        similarity_score = calculate_similarity(guess, target_word)
+        updated_guesses.append({
+            'guess': guess,
+            'similarity_score': round(similarity_score * 100)
+        })
+        
+        if similarity_score >= similarity_threshold:
+            game_over = True
+            message = f"Congratulations! '{guess}' is very close to the target word '{target_word}'!"
+            break
+    
+    if not game_over:
+        encouragement_messages = [
+            "Keep going! You're doing great!",
+            "Don't give up, you're getting closer!",
+            "Nice try! Keep guessing!",
+            "You're on the right track, keep it up!",
+            "Almost there! Keep thinking!"
+        ]
+        message = random.choice(encouragement_messages)
+    
+    return updated_guesses, game_over, message
+
 if __name__ == '__main__':
     app.run_server(debug=True)
 
